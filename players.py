df416d473a60b9429b34259f7197f14cdbd4b1d9
jaydenlaturnus <jaydenlaturnus@gmail.com>
2019-01-15 13:21:32 -0700

    Added support for projected points based off last 5 games

--- players.py
+++ players.py
@@ -8,9 +8,25 @@ from bs4 import BeautifulSoup
 class Players:
 
     urls = {
+        'gamelog': 'https://statsapi.web.nhl.com/api/v1/people/{id}/stats?stats=gameLog&season=20182019',
         'team': 'https://statsapi.web.nhl.com/api/v1/teams/{id}',
         'roster': 'https://statsapi.web.nhl.com/api/v1/teams/{id}/roster',
         'salary': 'https://rotogrinders.com/lineups/nhl?site=fanduel'
+    }
+
+    scoring = {
+        'assists': 8,
+        'goals': 12,
+        'shots': 1.6,
+        'blocked': 1.6,
+        'powerPlayPoints': 0.5,
+        'powerPlayGoals': 0.5,
+        'shortHandedPoints': 2,
+        'shortHandedGoals': 2,
+        'goalsAgaisnt': -4,
+        'shutouts': 8,
+        'saves': 0.8,
+        'decision': 12
     }
 
     def __init__(self, teams):
@@ -55,6 +71,33 @@ class Players:
 
         return temp_players
 
+    def projected_points(self, id):
+        url = self.urls['gamelog'].format(id=id)
+        page = json.loads(requests.get(url).text)
+
+        fpp = 0
+
+        stat_len = len(page['stats'][0]['splits'])
+
+        if (len(page['stats'][0]['splits']) < 5):
+            stat_len = len(page['stats'][0]['splits'])-1
+        else:
+            stat_len = 5
+
+        for i in range(0, stat_len):
+            for key in self.scoring:
+                if (key in page['stats'][0]['splits'][i]['stat']):
+                    if (key == 'decision'):
+                        if (page['stats'][0]['splits'][i]['stat'][key] == 'W'):
+                            val = self.scoring[key]
+                    else:
+                        val = page['stats'][0]['splits'][i]['stat'][key] * self.scoring[key]
+
+                    fpp += val
+
+        return round(fpp/5, 2)
+
+
     def find_roster(self):
         for team in self.teams:
             id = self.teams[team]['id']
@@ -68,9 +111,11 @@ class Players:
             # Checks current lineup and combines the id and salary under the player name
             for player in range(0, len(page['roster'])-1):
                 if (page['roster'][player]['person']['fullName'] in temp_players):
+                    fpp = self.projected_points(page['roster'][player]['person']['id'])
                     players[page['roster'][player]['person']['fullName']] = {
                         'id': page['roster'][player]['person']['id'],
-                        'salary': temp_players[page['roster'][player]['person']['fullName']]
+                        'salary': temp_players[page['roster'][player]['person']['fullName']],
+                        'fpp': fpp
                     }
             
             # Player lineups are put as the value for each team currently playing
